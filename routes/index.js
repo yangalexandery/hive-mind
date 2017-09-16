cdb8ec10f418bbf66e23a612cc76e6286320498f
Alok Puranik <alokpuranik1@gmail.com>
2017-09-16 13:51:32 -0400

    Attempt sockets

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
-var express = require('express');
-var router = express.Router();
+var app = require('express')();
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-router.get('/', function(req, res) {
+app.get('/', function(req, res) {
 	var path = require('path');
   	res.sendFile(path.resolve('public/html/index.html'));
-})
+});
 
-router.get('/game', function(req, res) {
+app.get('/game', function(req, res) {
 	res.send();
 	// req.params.gameid
-})
+});
 
-module.exports = router;
+io.on('connection', function(socket) {
+    console.log('Connection received');
+});
+
+http.listen(3000, function() {
+    console.log('*** Listening on 3000 ***');
+});
