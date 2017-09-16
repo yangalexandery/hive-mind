4770d48651f826695ff1444c4f5f2fa1e3ef29cf
Alok Puranik <alokpuranik1@gmail.com>
2017-09-16 15:38:05 -0400

    Basic websocket communication layer

--- public/js/moveprotocol.js
+++ public/js/moveprotocol.js
@@ -1,7 +1,35 @@
 var socket = io.connect("/");
 
-function start_game(roomId) {
+var room = 0;
 
+function start_game(roomId) {
+    room = roomId;
 }
 
-// vim: noexpandtab
+/*
+ * Send a move to the webserver
+ * from: square the piece is moving from, in chess notation
+ * to: square the piece is moving to, in chess notation
+ * promotion (optional): if a pawn is getting promoted, the algebraic notation
+ *      for the new piece - one of QRNB
+ */
+function sendMove(from, to, promote) {
+    var posRegex = /^[a-h][1-8]$/;
+    if (!from.match(posRegex)) {
+        throw "Invalid 'from' position: " + from;
+    }
+    if (!to.match(posRegex)) {
+        throw "Invalid 'to' position: " + to;
+    }
+    if ((promote !== undefined) && (!promote.match(/^[QRNB]$/))) {
+        throw "Invalid pawn promotion string: " + promote;
+    }
+
+    payload = {
+        "from" : from,
+        "to": to,
+        "promote": promote
+    };
+    socket.emit('move', payload);
+}
+
