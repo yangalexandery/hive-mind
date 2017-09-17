db164d560b0ccb4833392f6e5720eb9ad55f912e
eureka1123 <xl.elaine.guo@gmail.com>
2017-09-16 21:06:52 -0400

    redirection

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,18 @@
 function toGame(color){
+	console.log(color)
+	var data = {}
+	data.color= color
 	$.ajax({
     url: '/game',
     method: 'GET',
     // data: dataField,
+    data: data,
     success: function (data) {
-      window.location.reload(true);
-    }
+      window.location.href = "/game"
+    },
+    error: function(xhr, status, error) {
+      console.log(error);
+    },
+ 		async: false
   });
-	if(color=="red"){
-
-	}else if (color=="blue"){
-
-	}else{
-
-	}
 }
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res) {
   res.sendFile(path.resolve('public/html/index.html'));
 });
 
-router.get('/game', function(req, res) {
+router.get('/game', function(req, res,next) {
   res.render('game', {});
   // req.params.gameid
 });
