830565d72c05646b407dc5425a4c63f3f515d3b7
Linda Gong <lindagong@Lindas-MacBook-Air.local>
2017-09-17 01:47:57 -0400

    df

--- public/js/testchat.js
+++ public/js/testchat.js
@@ -1,11 +1,16 @@
 const CHANNEL_ID = 'mJEQDxDqFPDasNmw';
+// const drone = new ScaleDrone(CHANNEL_ID, {
+// 	data: {
+// 		name: getRandomName(),
+// 		color: getRandomColor(),
+// 	},
+// });
 const drone = new ScaleDrone(CHANNEL_ID, {
 	data: {
 		name: getRandomName(),
 		color: getRandomColor(),
 	},
 });
-
 function getRandomName() {
 	const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
 	const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
@@ -32,18 +37,18 @@ drone.on('open', error => {
 
 	room.on('members', m => {
 		members = m;
-		updateMembersDOM();
+		// updateMembersDOM();
 	});
 
 	room.on('member_join', member => {
 		members.push(member);
-		updateMembersDOM();
+		// updateMembersDOM();
 	});
 
 	room.on('member_leave', ({id}) => {
 		const index = members.findIndex(member => member.id === id);
 		members.splice(index, 1);
-		updateMembersDOM();
+		// updateMembersDOM();
 	});
 
 	room.on('data', (text, member) => {
@@ -94,13 +99,13 @@ function createMemberElement(member) {
 	return el;
 }
 
-function updateMembersDOM() {
-	DOM.membersCount.innerText = `${members.length} users in room:`;
-	DOM.membersList.innerHTML = '';
-	members.forEach(member =>
-		DOM.membersList.appendChild(createMemberElement(member))
-	);
-}
+// function updateMembersDOM() {
+// 	DOM.membersCount.innerText = `${members.length} users in room:`;
+// 	DOM.membersList.innerHTML = '';
+// 	members.forEach(member =>
+// 		DOM.membersList.appendChild(createMemberElement(member))
+// 	);
+// }
 
 function createMessageElement(text, member) {
 	const el = document.createElement('div');
